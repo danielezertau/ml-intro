b51394104a70abf228f49d42db0c853122a6fac3
danielezertau <danielezer@mail.tau.ac.il>
2022-03-04 14:46:16 +0200

    Add code for q2d

--- hw1/q2.py
+++ hw1/q2.py
@@ -3,16 +3,20 @@ from sklearn.datasets import fetch_openml
 from matplotlib import pyplot
 
 
-def pre_processing(num_train_images):
-    num_images = 11000
+def get_mnist_data():
     mnist = fetch_openml('mnist_784', as_frame=False)
     data = mnist['data']
     labels = mnist['target']
+    return data, labels
+
+
+def get_train_and_test_sets(data, labels, num_train_images, num_test_images=1000):
+    num_images = num_train_images + num_test_images
     idx = numpy.random.RandomState(0).choice(70000, num_images)
     train = data[idx[:num_train_images], :].astype(int)
     train_labels = labels[idx[:num_train_images]]
-    test = data[idx[10000:], :].astype(int)
-    test_labels = labels[idx[10000:]]
+    test = data[idx[num_images - num_test_images:], :].astype(int)
+    test_labels = labels[idx[num_images - num_test_images:]]
     return train, train_labels, test, test_labels
 
 
@@ -39,9 +43,27 @@ def q2c(train, train_labels, test, test_labels):
     pyplot.plot(ks, results)
     pyplot.savefig('q2c.png')
     pyplot.show()
+    return results.argmax(), numpy.max(results)
+
+
+def q2d(data, labels):
+    def accuracy(num_train_images):
+        train, train_labels, test, test_labels = get_train_and_test_sets(data, labels, num_train_images)
+        return accuracy_for_k(1, train, train_labels, test, test_labels)
+    ns = numpy.arange(100, 5001, 100)
+    results = numpy.array([accuracy(n) for n in ns])
+    pyplot.xlabel('N')
+    pyplot.ylabel('Predictor accuracy')
+    pyplot.plot(ns, results)
+    pyplot.savefig('q2d.png')
+    pyplot.show()
 
 
 if __name__ == '__main__':
-    num_train_images1 = 1000
-    train1, train_labels1, test1, test_labels1 = pre_processing(num_train_images1)
-    print(q2c(train1, train_labels1, test1, test_labels1))
+    n_train_images = 1000
+    images, images_labels = get_mnist_data()
+    train_images, train_images_labels, test_images, test_images_labels = get_train_and_test_sets(images, images_labels,
+                                                                                                 n_train_images)
+    print(q2b(train_images, train_images_labels, test_images, test_images_labels))
+    print(q2c(train_images, train_images_labels, test_images, test_images_labels))
+    q2d(images, images_labels)

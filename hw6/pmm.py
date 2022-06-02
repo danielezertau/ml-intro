98cad3ee7d0cdb74bc6d4ac0233839a1210e8b89
danielezertau <danielezer@mail.tau.ac.il>
2022-06-03 00:11:55 +0300

    Improve PMM EM performance by using matrices and numpy for calculating the interim values needed for deriving the update

--- hw6/pmm.py
+++ hw6/pmm.py
@@ -1,10 +1,11 @@
+from typing import List
 import matplotlib.pyplot as plt
 from scipy.stats import poisson
 import numpy as np
 
 
-REAL_CS = [0.4, 0.4, 0.2]
-REAL_LAMBS = [5, 10, 18]
+REAL_CS = np.array([0.4, 0.4, 0.2])
+REAL_LAMBS = np.array([5, 10, 18])
 
 
 def generate_data_from_pmm(num_samples, cs, lambdas):
@@ -16,12 +17,8 @@ def q2a():
     return generate_data_from_pmm(1000, REAL_CS, REAL_LAMBS)
 
 
-def r(x: int, lam: int, c: float) -> float:
-    return poisson.pmf(x, lam) * c
-
-
-def p(x, z, lam, c):
-    return r(x, lam[z], c[z]) / np.sum(np.vectorize(r, excluded=['x'])(x=x, lam=lam, c=c))
+def p(x: int, lam: List[int], c: List[float]) -> List[float]:
+    return poisson.pmf(x, lam) * c / np.sum(poisson.pmf(x, lam) * c)
 
 
 def em(sample, num_iterations):
@@ -29,16 +26,11 @@ def em(sample, num_iterations):
     k = 3
     c_t = np.ones(k) * (1/k)
     lam_t = np.arange(start=1, stop=k + 1)
-    c_t_p_1 = np.zeros(k)
-    lam_t_p_1 = np.zeros(k)
     for t in range(num_iterations):
-        for z in range(len(lam_t)):
-            ps = np.vectorize(p, excluded=['z', 'lam', 'c'])(sample, z=z, lam=lam_t, c=c_t)
-            p_sum_over_x = np.sum(ps)
-            lam_t_p_1[z] = np.sum(ps * sample) / p_sum_over_x
-            c_t_p_1[z] = p_sum_over_x / n
-        lam_t = lam_t_p_1
-        c_t = c_t_p_1
+        ps = np.vectorize(p, excluded=['lam', 'c'], signature='()->(n)')(sample, lam=lam_t, c=c_t)
+        p_sum_over_x = np.sum(ps, axis=0)
+        lam_t = np.sum(ps.T * sample, axis=1) / p_sum_over_x
+        c_t = p_sum_over_x / n
 
     return np.array(lam_t), np.array(c_t)
 
@@ -55,9 +47,8 @@ def q2c():
     xs = np.arange(50)
     for t in ts:
         lams, cs = em(sample, t)
-        em_ys = np.vectorize(lambda x: np.sum([cs[i] * poisson.pmf(x, lams[i]) for i in range(len(lams))]))(xs)
-        real_ys = np.vectorize(lambda x: np.sum([REAL_CS[i] * poisson.pmf(x, REAL_LAMBS[i])
-                                                 for i in range(len(REAL_LAMBS))]))(xs)
+        em_ys = np.vectorize(lambda x: np.sum(cs * poisson.pmf(x, lams)))(xs)
+        real_ys = np.vectorize(lambda x: np.sum(REAL_CS * poisson.pmf(x, REAL_LAMBS)))(xs)
         plt.title(f"t = {t}")
         plt.xlabel("x")
         plt.ylabel("P(X=x)")
@@ -70,4 +61,3 @@ def q2c():
 
 if __name__ == '__main__':
     q2c()
-    print("HI")
